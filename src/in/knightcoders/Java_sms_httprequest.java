469a995023dc8a0632a4f29493ef7b479ad24ec3
Somendra Singh Rajput <singhsomendra420@gmail.com>
2021-08-10 14:35:47 +0530

    Adding Sms Sender Project Using Java

---
+++ src/in/knightcoders/Java_sms_httprequest.java
@@ -0,0 +1,14 @@
+package in.knightcoders;
+
+public class Java_sms_httprequest {
+
+	public static void main(String[] args) {
+		
+		SendSms ss = new SendSms();
+		
+		ss.sendSMS();
+		
+		System.out.println("Message sent.");
+	}
+
+}
